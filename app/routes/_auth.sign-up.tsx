5028fede09dd9e8514a39e4f93a491faef2f7feb
Yash Dhingra <77784099+yash-dhingra@users.noreply.github.com>
2024-10-01 12:40:25 +0530

    Update _auth.sign-up.tsx

--- app/routes/_auth.sign-up.tsx
+++ app/routes/_auth.sign-up.tsx
@@ -18,7 +18,7 @@ const  FormComponent: React.FC = () => {
     };
 
     return (
-        <div className="flex justify-center items-center min-h-max">
+        <div className="flex justify-center min-h-lvh p-10">
             <form className="w-full max-w-lg font-sans">
                 <div className="flex flex-wrap -mx-3 mb-6">
                     <div className="w-full md:w-1/2 px-3 mb-6 md:mb-0">
@@ -81,11 +81,11 @@ const  FormComponent: React.FC = () => {
                         <div className="flex flex-col items-center justify-center w-full">
                             <label
                                 htmlFor="dropzone-file"
-                                className="h-28 flex flex-col items-center justify-center w-full h-64 border-2 border-gray-300 border-dashed rounded-lg cursor-pointer bg-gray-50 dark:bg-gray-700 hover:bg-gray-100 dark:border-gray-600 dark:hover:border-gray-500 dark:hover:bg-gray-600"
+                                className="h-28 flex flex-col items-center justify-center w-full border-2 border-gray-300 border-dashed rounded-lg cursor-pointer bg-gray-50  hover:bg-gray-100"
                             >
                                 <div className="flex flex-col items-center justify-center pt-5 pb-6">
                                     <svg
-                                        className="w-8 h-8 mb-4 text-gray-500 dark:text-gray-400"
+                                        className="w-8 h-8 mb-4 text-gray-500"
                                         aria-hidden="true"
                                         xmlns="http://www.w3.org/2000/svg"
                                         fill="none"
@@ -99,10 +99,10 @@ const  FormComponent: React.FC = () => {
                                             d="M13 13h3a3 3 0 0 0 0-6h-.025A5.56 5.56 0 0 0 16 6.5 5.5 5.5 0 0 0 5.207 5.021C5.137 5.017 5.071 5 5 5a4 4 0 0 0 0 8h2.167M10 15V6m0 0L8 8m2-2 2 2"
                                         />
                                     </svg>
-                                    <p className="mb-2 text-sm text-gray-500 dark:text-gray-400">
+                                    <p className="mb-2 text-sm text-gray-500">
                                         <span className="font-semibold">Click to upload</span> or drag and drop
                                     </p>
-                                    <p className="text-xs text-gray-500 dark:text-gray-400">
+                                    <p className="text-xs text-gray-500">
                                         SVG, PNG, JPG or GIF (MAX. 800x400px)
                                     </p>
                                 </div>
@@ -238,7 +238,6 @@ const  FormComponent: React.FC = () => {
                 </div>
             </form>
         </div>
-
     )
 }
 export default FormComponent;
