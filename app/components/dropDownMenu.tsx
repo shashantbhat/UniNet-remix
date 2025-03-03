3492bf129b2758a33854bea177b2b20a72ac6eff
Yash Dhingra <77784099+yash-dhingra@users.noreply.github.com>
2025-03-03 17:36:41 +0530

    Minor Bug Fixes Done

--- app/components/dropDownMenu.tsx
+++ app/components/dropDownMenu.tsx
@@ -6,17 +6,23 @@ interface DropdownMenuProps {
   onSelectTag: (tagId: number) => void;
 }
 
-const DropdownMenu: React.FC<DropdownMenuProps> = ({ tags, selectedTags, onSelectTag }) => {
+const DropdownMenu: React.FC<DropdownMenuProps> = ({
+  tags,
+  selectedTags,
+  onSelectTag,
+}) => {
   return (
-    <div className="absolute top-full left-0 mt-2 w-full bg-white border border-gray-300 rounded-lg shadow-lg z-50">
-      <div className="flex justify-center items-center py-2">
-        <span className="text-center">Search using tags:</span>
+    <div className="absolute top-full left-0 mt-2 w-full bg-white border border-gray-300 rounded-xl text-sm shadow-lg z-50">
+      <div className="flex items-center px-2 pt-4">
+        <span className="font-medium">Search using tags:</span>
       </div>
-      <ul className="py-2">
+      <ul className="flex flex-wrap py-2">
         {tags.map((tag) => (
           <li
             key={tag.id}
-            className={`px-3 py-1 m-1 hover:bg-gray-400 bg-gray-200 cursor-pointer rounded-full ${selectedTags.includes(tag.id) ? 'bg-gray-400' : ''}`}
+            className={`px-3 py-1 m-1 hover:bg-gray-400 bg-gray-200 cursor-pointer rounded-full ${
+              selectedTags.includes(tag.id) ? "bg-gray-400" : ""
+            }`}
             onClick={() => onSelectTag(tag.id)}
           >
             {tag.name}
--- app/routes/dash._student.$id.community-operated.tsx
+++ app/routes/dash._student.$id.community-operated.tsx
@@ -98,45 +98,80 @@ const CommunityOperated = () => {
               window.location.href = `/dash/${id}/upload-material`;
             }}
           >
-            <svg 
-              xmlns="http://www.w3.org/2000/svg" 
-              width="20" 
-              height="20" 
-              fill="currentColor" 
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              width="20"
+              height="20"
+              fill="currentColor"
               viewBox="0 0 256 256"
             >
               <path d="M213.66,82.34l-56-56A8,8,0,0,0,152,24H56A16,16,0,0,0,40,40V216a16,16,0,0,0,16,16H200a16,16,0,0,0,16-16V88A8,8,0,0,0,213.66,82.34ZM160,51.31,188.69,80H160ZM200,216H56V40h88V88a8,8,0,0,0,8,8h48V216Zm-40-64a8,8,0,0,1-8,8H136v16a8,8,0,0,1-16,0V160H104a8,8,0,0,1,0-16h16V128a8,8,0,0,1,16,0v16h16A8,8,0,0,1,160,152Z"></path>
             </svg>
             <span>Add File</span>
           </button>
-          
-          <div className="relative flex items-center gap-2 w-1/3 min-w-[300px]">
-            <input
-              type="text"
-              placeholder="Search files"
-              value={searchQuery}
-              onChange={handleSearchChange}
-              onClick={handleSearchClick}
-              className="w-full px-4 py-2.5 border rounded-lg focus:outline-none focus:ring-1 focus:ring-black transition-all hover:border-black duration-200"
-            />
-            {isDropdownOpen && (
-              <DropdownMenu tags={tags} selectedTags={selectedTags} onSelectTag={handleSelectTag} />
-            )}
+
+          <div className="flex items-center gap-2">
+            <button
+              className="flex items-center gap-2 border border-black bg-white text-black hover:bg-black hover:text-white py-2.5 px-4 rounded-xl text-sm font-medium transition"
+              onClick={() => {
+                window.location.href = `/dash/${id}/upload-material`;
+              }}
+            >
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                width="20"
+                height="20"
+                fill="currentColor"
+                viewBox="0 0 256 256"
+              >
+                <path d="M213.66,82.34l-56-56A8,8,0,0,0,152,24H56A16,16,0,0,0,40,40V216a16,16,0,0,0,16,16H200a16,16,0,0,0,16-16V88A8,8,0,0,0,213.66,82.34ZM160,51.31,188.69,80H160ZM200,216H56V40h88V88a8,8,0,0,0,8,8h48V216Zm-40-64a8,8,0,0,1-8,8H136v16a8,8,0,0,1-16,0V160H104a8,8,0,0,1,0-16h16V128a8,8,0,0,1,16,0v16h16A8,8,0,0,1,160,152Z"></path>
+              </svg>
+              <span>Adv Search</span>
+            </button>
+            <div className="relative flex items-center gap-2 w-1/3 min-w-[400px]">
+              <input
+                type="text"
+                placeholder="Search files"
+                value={searchQuery}
+                onChange={handleSearchChange}
+                onClick={handleSearchClick}
+                className="w-full py-2.5 px-4 rounded-xl text-sm font-medium focus:outline-none focus:ring-1 focus:ring-black transition-all hover:border-black duration-200"
+              />
+              {isDropdownOpen && (
+                <DropdownMenu
+                  tags={tags}
+                  selectedTags={selectedTags}
+                  onSelectTag={handleSelectTag}
+                />
+              )}
+            </div>
           </div>
         </div>
         <div className="flex-1 overflow-auto">
           <table className="min-w-full bg-white table-fixed">
             <thead>
-              <tr className="h-12"> {/* Sets uniform row height */}
-                <th className="py-2 px-4 border-b text-center w-40">File Name</th>
-                <th className="py-2 px-4 border-b text-center w-64">Description</th>
-                <th className="py-2 px-4 border-b text-center w-36">Upload Date</th>
-                <th className="py-2 px-4 border-b text-center w-32">Average Rating</th>
+              <tr className="h-12">
+                {" "}
+                {/* Sets uniform row height */}
+                <th className="py-2 px-4 border-b text-center w-40">
+                  File Name
+                </th>
+                <th className="py-2 px-4 border-b text-center w-64">
+                  Description
+                </th>
+                <th className="py-2 px-4 border-b text-center w-36">
+                  Upload Date
+                </th>
+                <th className="py-2 px-4 border-b text-center w-32">
+                  Average Rating
+                </th>
               </tr>
             </thead>
             <tbody>
               {filteredFiles?.map((file: any) => (
-                <tr key={file.id} className="h-10"> {/* Ensures uniform row height */}
+                <tr key={file.id} className="h-10">
+                  {" "}
+                  {/* Ensures uniform row height */}
                   <td className="py-2 px-2 border-b text-center w-40 truncate">
                     <a
                       href={`/dash/id/files/${file.id}`}
